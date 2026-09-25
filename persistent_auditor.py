a4654c8afbf650e28d9585c07f3120e8307c0761
Vyngamer <2603199@sit.singaporetech.edu.sg>
2026-09-25 09:55:28 +0800

    added load inventory function

--- persistent_auditor.py
+++ persistent_auditor.py
@@ -5,3 +5,12 @@ def save_inventory(new_entry):
     for i in new_entry:
         with open("orders.txt", "a") as inventory_file:
             inventory_file.write(i + "\n")
+
+def load_inventory():
+    if not os.path.exists("orders.txt"):
+        # create an empty file so future appends work
+        open("orders.txt", "w").close()
+        return []
+    with open("orders.txt", "r") as inventory_file:
+        inventory = inventory_file.read().splitlines()
+    return inventory
