0f6d5f2ea47b59fd15dbe2b114183b13c4bb3508
Vyngamer <2603199@sit.singaporetech.edu.sg>
2026-09-25 09:54:22 +0800

    added save inventory function

--- persistent_auditor.py
+++ persistent_auditor.py
@@ -1,1 +1,7 @@
-print("Hello world")
+import os
+
+# function to save inventory
+def save_inventory(new_entry):
+    for i in new_entry:
+        with open("orders.txt", "a") as inventory_file:
+            inventory_file.write(i + "\n")
